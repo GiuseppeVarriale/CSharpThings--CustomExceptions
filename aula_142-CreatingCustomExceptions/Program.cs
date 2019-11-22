4c0f6ab74163477d75194b7ee97847169ab864b9
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-22 10:21:02 -0300

    Add some Logic to Program.cs

--- aula_142-CreatingCustomExceptions/Program.cs
+++ aula_142-CreatingCustomExceptions/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-
+using aula_142_CreatingCustomExceptions.Entities;
 namespace aula_142_CreatingCustomExceptions
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd//MM/yyyy): ");
+            DateTime checkInDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-ou date (dd//MM/yyyy)): ");
+            DateTime checkOutDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            Reservation reservation = new Reservation(roomNumber, checkInDate, checkOutDate);
+            Console.WriteLine(reservation);
+
         }
     }
 }
