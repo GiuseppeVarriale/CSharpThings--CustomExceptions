27fe70082af5edafc4be201b0e30842ecc5d60db
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-22 09:01:38 -0300

    Create Reservation Class

---
+++ aula_142-CreatingCustomExceptions/Entities/Reservation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace aula_142_CreatingCustomExceptions.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; private set; }
+        public DateTime CheckIn { get; private set; }
+        public DateTime CheckOut { get; private set; }
+
+        public Reservation() 
+        { 
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration() 
+        {
+            TimeSpan duration = CheckIn.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + Duration()
+                + " nights";
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout) 
+        {
+            Console.WriteLine("not Implemented yet") ;
+        }
+    }
+}
